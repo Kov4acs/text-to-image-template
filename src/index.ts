418e9d67bcd745f4302aedcea14a904409514b54
Kov4acs <108982530+Kov4acs@users.noreply.github.com>
2025-11-10 17:33:19 +0100

    Change prompt from 'cyberpunk cat' to 'scooby snack'

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "scooby snack",
     };
 
     const response = await env.AI.run(
